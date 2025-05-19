849524062f6bdeffef9d4b2dcc1fb71b2aa6f2cf
3mad Hany <144627820+3madhani@users.noreply.github.com>
2025-05-19 20:26:39 +0300

    feat: Add image picker functionality and file selector support for product images

--- lib/feature/add_product/presentation/view/widgets/image_field.dart
+++ lib/feature/add_product/presentation/view/widgets/image_field.dart
@@ -1,17 +1,64 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ImageField extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+import '../../../../../core/utils/app_colors.dart';
+
+class ImageField extends StatefulWidget {
   const ImageField({super.key});
 
   @override
+  State<ImageField> createState() => _ImageFieldState();
+}
+
+class _ImageFieldState extends State<ImageField> {
+  bool isLoading = false;
+  File? fileImage;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(12),
+    return Skeletonizer(
+      enabled: isLoading,
+      child: GestureDetector(
+        onTap: () async {
+          isLoading = true;
+          setState(() {});
+          try {
+            final ImagePicker picker = ImagePicker();
+            // Pick an image.
+            final XFile? image = await picker.pickImage(
+              source: ImageSource.gallery,
+            );
+            if (image != null) {
+              fileImage = File(image.path);
+            }
+          } on Exception catch (e) {
+            // TODO
+          }
+          isLoading = false;
+          setState(() {});
+        },
+        child: Container(
+          height: 220,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(color: AppColors.primaryLightColor),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child:
+              fileImage != null
+                  ? ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.file(fileImage!, fit: BoxFit.fill),
+                  )
+                  : const Icon(
+                    size: 180,
+                    Icons.image_rounded,
+                    color: Colors.grey,
+                  ),
+        ),
       ),
-      child: const Icon(size: 180, Icons.image_rounded, color: Colors.grey),
     );
   }
 }

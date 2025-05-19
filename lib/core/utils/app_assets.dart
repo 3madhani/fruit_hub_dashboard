80c0204d0f8c4cf9c18bdd412237f30c37197104
3mad Hany <144627820+3madhani@users.noreply.github.com>
2025-05-19 16:15:58 +0300

    Add SVG assets and localization support
    - Added search.svg and splash_bottom.svg to the assets/svg directory.
    - Created app_assets.dart to manage asset paths for images and SVGs.
    - Implemented backend_endpoints.dart for user data API endpoints.
    - Generated localization files including messages_all.dart and messages_en.dart for English language support.
    - Updated l10n.dart for localization management.
    - Added an empty intl_en.arb file for future localization strings.

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,105 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesProfileImage
+  /// assets/images/profile_image.png
+  static const String imagesProfileImage = "assets/images/profile_image.png";
+
+  /// Assets for imagesStrawberryTest
+  /// assets/images/Strawberry_test.png
+  static const String imagesStrawberryTest = "assets/images/Strawberry_test.png";
+
+  /// Assets for svgActiveHome
+  /// assets/svg/active-home.svg
+  static const String svgActiveHome = "assets/svg/active-home.svg";
+
+  /// Assets for svgActiveProducts
+  /// assets/svg/active_products.svg
+  static const String svgActiveProducts = "assets/svg/active_products.svg";
+
+  /// Assets for svgActiveShoppingCart
+  /// assets/svg/active-shopping-cart.svg
+  static const String svgActiveShoppingCart = "assets/svg/active-shopping-cart.svg";
+
+  /// Assets for svgActiveUser
+  /// assets/svg/active_user.svg
+  static const String svgActiveUser = "assets/svg/active_user.svg";
+
+  /// Assets for svgAppleIcons
+  /// assets/svg/apple_Icons.svg
+  static const String svgAppleIcons = "assets/svg/apple_Icons.svg";
+
+  /// Assets for svgBackground1
+  /// assets/svg/background_1.svg
+  static const String svgBackground1 = "assets/svg/background_1.svg";
+
+  /// Assets for svgBackground2
+  /// assets/svg/background_2.svg
+  static const String svgBackground2 = "assets/svg/background_2.svg";
+
+  /// Assets for svgCheck
+  /// assets/svg/Check.svg
+  static const String svgCheck = "assets/svg/Check.svg";
+
+  /// Assets for svgFacebookIcons
+  /// assets/svg/facebook_Icons.svg
+  static const String svgFacebookIcons = "assets/svg/facebook_Icons.svg";
+
+  /// Assets for svgFeaturedItemBackground
+  /// assets/svg/featured_item_background.svg
+  static const String svgFeaturedItemBackground = "assets/svg/featured_item_background.svg";
+
+  /// Assets for svgFilter
+  /// assets/svg/filter.svg
+  static const String svgFilter = "assets/svg/filter.svg";
+
+  /// Assets for svgGoogleIcons
+  /// assets/svg/google_Icons.svg
+  static const String svgGoogleIcons = "assets/svg/google_Icons.svg";
+
+  /// Assets for svgImage1
+  /// assets/svg/image_1.svg
+  static const String svgImage1 = "assets/svg/image_1.svg";
+
+  /// Assets for svgImage2
+  /// assets/svg/image_2.svg
+  static const String svgImage2 = "assets/svg/image_2.svg";
+
+  /// Assets for svgInactiveHome
+  /// assets/svg/inactive-home.svg
+  static const String svgInactiveHome = "assets/svg/inactive-home.svg";
+
+  /// Assets for svgInactiveProducts
+  /// assets/svg/inactive-products.svg
+  static const String svgInactiveProducts = "assets/svg/inactive-products.svg";
+
+  /// Assets for svgInactiveShoppingCart
+  /// assets/svg/inactive-shopping-cart.svg
+  static const String svgInactiveShoppingCart = "assets/svg/inactive-shopping-cart.svg";
+
+  /// Assets for svgInactiveUser
+  /// assets/svg/inactive-user.svg
+  static const String svgInactiveUser = "assets/svg/inactive-user.svg";
+
+  /// Assets for svgLogo
+  /// assets/svg/logo.svg
+  static const String svgLogo = "assets/svg/logo.svg";
+
+  /// Assets for svgNotification
+  /// assets/svg/notification.svg
+  static const String svgNotification = "assets/svg/notification.svg";
+
+  /// Assets for svgPlant
+  /// assets/svg/plant.svg
+  static const String svgPlant = "assets/svg/plant.svg";
+
+  /// Assets for svgSearch
+  /// assets/svg/search.svg
+  static const String svgSearch = "assets/svg/search.svg";
+
+  /// Assets for svgSplashBottom
+  /// assets/svg/splash_bottom.svg
+  static const String svgSplashBottom = "assets/svg/splash_bottom.svg";
+}
+
